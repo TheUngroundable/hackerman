69f33087bd32ca9b74d4111881938e37b27dceba
TheUngroundable <35199075+TheUngroundable@users.noreply.github.com>
2020-02-01 00:10:50 +0100

    more player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,28 +5,44 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
-    public int maxJumps;
+    public int timesJumped;
     public float jumpSpeed;
+    public bool isGrounded;
+    public float deadZone;
+    private float distToGround;
     private Vector3 movementDirection;
     private Rigidbody rigidBody;
     // Start is called before the first frame update
     void Start(){
         rigidBody = GetComponent<Rigidbody>();
+        distToGround = transform.GetChild(0).GetComponent<Collider>().bounds.extents.y;
     }
 
     // Update is called once per frame
     void Update(){
         PlayerJump();
         float horizontal = Input.GetAxis("Horizontal");
-        Vector3 movement  = new Vector3(horizontal * speed * Time.deltaTime, 0, 0);
-        rigidBody.MovePosition(transform.position+movement);
+
+        if(Mathf.Abs(Input.GetAxis("Horizontal")) > deadZone){
+            Vector3 movement  = new Vector3(horizontal * speed * Time.deltaTime, 0, 0);
+            rigidBody.MovePosition(transform.position+movement);
+        }
     }
 
     void PlayerJump(){
-        if(Input.GetKeyDown("space") && maxJumps < 2){  
+        if(Input.GetKeyDown("space") && timesJumped < 1){  
             rigidBody.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
-            maxJumps++;
+            timesJumped++;
+        }
+        isGrounded = IsGrounded();
+        if(isGrounded){
+            timesJumped = 0;
         }
     }
+
+    bool IsGrounded() {
+        return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f);
+    }
+        
 
 }
