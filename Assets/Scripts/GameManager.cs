c6655a06e6c54aac83b56d90f2fa6b56bff11572
TheUngroundable <35199075+TheUngroundable@users.noreply.github.com>
2020-02-01 19:38:33 +0100

    GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public AudioClip softSong;
+    public AudioClip metalSong;
+    private AudioSource audioSource;
+    void Start() {
+        audioSource = GetComponent<AudioSource>();
+        PlayClip(softSong);
+    }
+
+    void PlayClip(AudioClip clip){
+        audioSource.clip = clip;
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
