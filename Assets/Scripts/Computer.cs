0e7eb2438de58ad54a259f15bf793277945aa2f9
TheUngroundable <35199075+TheUngroundable@users.noreply.github.com>
2020-02-02 01:00:16 +0100

    Fixed player strange mace behaviour

--- Assets/Scripts/Computer.cs
+++ Assets/Scripts/Computer.cs
@@ -4,22 +4,12 @@ using UnityEngine;
 
 public class Computer : MonoBehaviour{
     public bool repaired;
-    public float explosionRadius = 5;
-    private Rigidbody rb;
-
-    public float explosionForce = 850;
-
     private void Start()
     {
-        rb = GetComponent<Rigidbody>();
     }
     public void SetRepaired(bool repaired){
         this.repaired = repaired;
         TileManager.Instance.curRoom.CheckForDoor();
-        rb.isKinematic =false;
-        Vector3 rndForce = new Vector3(transform.localPosition.x + Random.Range(0, .3f), transform.localPosition.y + Random.Range(0, .3f), transform.localPosition.z + Random.Range(0, .3f));
-        rb.AddExplosionForce(explosionForce, transform.position + new Vector3(+1f,0,0), explosionRadius, 3.0F);
-        // Alert tile manager
     }
 
 }
--- Assets/Scripts/Mazza.cs
+++ Assets/Scripts/Mazza.cs
@@ -6,11 +6,19 @@ public class Mazza : MonoBehaviour
 {
 
     public Player player;
+    
+    public float explosionForce = 850;
+    public float explosionRadius = 5;
     void OnCollisionEnter(Collision collision) {
         if(player.isAttacking){
             
             MazzaAudioManager.Instance.PlayHit();
-            
+            if(collision.gameObject.GetComponent<Rigidbody>() != null){
+                Rigidbody collisionRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+                Vector3 rndForce = new Vector3(collision.transform.localPosition.x + Random.Range(0, .3f), collision.transform.localPosition.y + Random.Range(0, .3f), collision.transform.localPosition.z + Random.Range(0, .3f));
+                collisionRigidbody.AddExplosionForce(explosionForce, collision.transform.position + new Vector3(+1f,0,0), explosionRadius, 3.0F);
+            }
+        
             if (collision.gameObject.tag == "Computer" && !collision.gameObject.GetComponent<Computer>().repaired){
                 Computer computer = collision.gameObject.GetComponent<Computer>();
                 computer.SetRepaired(true);
